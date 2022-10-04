32d525752387217bb3e0994c5217227d50e254ce
Krunal Shigavan <krunal.shigavan@gmail.com>
2022-10-04 01:46:55 -0400

    Added Razor Pages - Services/About Us/Insights & Trends

---
+++ HimalayanLogistics/Pages/InsightsTrends.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HimalayanLogistics.Pages
+{
+    public class InsightsTrendsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ HimalayanLogistics/Pages/Services.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HimalayanLogistics.Pages
+{
+    public class ServicesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
